6c4caaa42dd29040240a6fa2f9d00adf448e5268
kimkeumbi <dubu941019@gmail.com>
2017-02-02 16:01:28 +0900

    report card : Show the sum of each grade
    Signed-off-by: kimkeumbi <dubu941019@gmail.com>

---
+++ commit5/report_card.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+	int kor, eng, math;
+	int sum = 0;
+
+	math = 100;
+	eng = 100;
+	kor = 50;
+	sum = 100 + 100 + 50;
+
+	printf("This program print report card.\n");
+
+	printf("Korean : %d\n", kor);
+	printf("English : %d\n", eng);
+	printf("Math : %d\n", math);
+	printf("Sum : %d\n", sum);
+	return 0;
+}
