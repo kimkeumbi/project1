f575a1a45ab31c2fba912b1979d8df1cc903f147
kimkeumbi <dubu941019@gmail.com>
2017-02-02 16:03:10 +0900

    report card : Get a average of grades
    Signed-off-by: kimkeumbi <dubu941019@gmail.com>

---
+++ commit6-1/report_card.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+	int kor, eng, math;
+	int average = 0;
+
+	math = 80;
+	eng = 100;
+	kor = 90;
+	average = (80 + 100 + 90)/3;
+
+	printf("This program print report card.\n");
+
+	printf("Korean : %d\n", kor);
+	printf("English : %d\n", eng);
+	printf("Math : %d\n", math);
+	printf("Sum : %d\n", sum);
+	printf("M : %d\n", sum/3);
+	return 0;
+}
